7861cc82bb568cea839f31a6c60a0dd8204892d0
ksturdivant <karl.sturdivant@fahlgren.com>
2017-12-18 20:10:02 +0000

    Initial commit.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Pixel Test</title>
+
+        <link href="css/main.css" media="all" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+
+        <div id="gridContainer"></div>
+        <button id="btn" onclick="Grid.buildPaths();">Run</button>
+
+        <script src="js/vendor/jquery.min.js" type="text/javascript"></script>
+        <script src="js/grid.js" type="text/javascript"></script>
+    </body>
+</html>
